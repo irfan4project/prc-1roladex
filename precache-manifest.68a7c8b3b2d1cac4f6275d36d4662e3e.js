c9f40f32d341e9cb00135060eb0140a482190c1c
irfan saeed <irfan4project@gmail.com>
2020-01-14 16:11:21 +0500

    Updates

---
+++ precache-manifest.68a7c8b3b2d1cac4f6275d36d4662e3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "861bbca09775f9e8dd0b93163dfd9cdb",
+    "url": "/prc-1roladex/index.html"
+  },
+  {
+    "revision": "71b95fb66f622e5665a2",
+    "url": "/prc-1roladex/static/css/main.94b04abe.chunk.css"
+  },
+  {
+    "revision": "c728ba4a2366e885ec54",
+    "url": "/prc-1roladex/static/js/2.d8d20882.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/prc-1roladex/static/js/2.d8d20882.chunk.js.LICENSE"
+  },
+  {
+    "revision": "71b95fb66f622e5665a2",
+    "url": "/prc-1roladex/static/js/main.dc37c8b5.chunk.js"
+  },
+  {
+    "revision": "3d23a0a83f6745a839d1",
+    "url": "/prc-1roladex/static/js/runtime-main.d9b5e361.js"
+  }
+]);
